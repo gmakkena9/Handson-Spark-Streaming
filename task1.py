63ffee00010e9f4a78e4df0ec58af6da9b095170
Gopi Bharath Makkena <gmakkena@charlotte.edu>
2026-06-08 16:00:36 -0400

    Complete Tasks 1, 2, and 3 with output CSV files

--- task1.py
+++ task1.py
@@ -1,9 +1,10 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
-from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType
 
 # Create a Spark session
 spark = SparkSession.builder.appName("RideSharingAnalytics").getOrCreate()
+spark.sparkContext.setLogLevel("ERROR")
 
 # Define the schema for incoming JSON data
 schema = StructType([
@@ -15,9 +16,23 @@ schema = StructType([
 ])
 
 # Read streaming data from socket
+raw_stream = spark.readStream.format("socket") \
+    .option("host", "localhost") \
+    .option("port", 9999) \
+    .load()
 
 # Parse JSON data into columns using the defined schema
+parsed_df = raw_stream.select(
+    from_json(col("value"), schema).alias("data")
+).select("data.*")
 
 # Print parsed data to the CSV files
+def write_to_csv(batch_df, batch_id):
+    batch_df.write.mode("append").option("header", True).csv("outputs/task_1")
+
+query = parsed_df.writeStream \
+    .foreachBatch(write_to_csv) \
+    .outputMode("append") \
+    .start()
 
 query.awaitTermination()
